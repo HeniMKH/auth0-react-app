1ff1625364b28019a94f2996727738d26b3054e8
Hengameh M.Khani <afsooneday@gmail.com>
2023-07-25 13:02:08 +0100

    loading-Done

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,22 +1,64 @@
+// import { useAuth0 } from '@auth0/auth0-react';
+// import React from 'react'
+
+// const Profile = () => {
+//     const { user, isAuthenticated } = useAuth0();
+    
+//   return (
+//     isAuthenticated && (
+//       <article className='column'>
+//       {/* {user?.picture && <img src={user.picture} alt={user?.name} />}
+//   //  <h2>{user?.name}</h2> */}
+//       <ul>
+//       {Object.keys(user).map((objKey, i) => {
+//             if (objKey === 'picture') {
+//               return <img key={i} src={user[objKey]} alt={user?.name} />;
+//             } else {
+//              return <li key={i}>{objKey}: {user[objKey]}</li>;
+//             }
+//           })}
+//       </ul>
+//   </article>
+
+//     )
+   
+//   )
+// }
+
+// export default Profile
+
 import { useAuth0 } from '@auth0/auth0-react';
-import React from 'react'
+import React from 'react';
 
 const Profile = () => {
-    const { user, isAuthenticated } = useAuth0();
+  const { user, isAuthenticated, logout } = useAuth0();
+
   return (
-    isAuthenticated && (
-      <article className= 'column'>
-        {user?.picture && <img src={user.picture} alt={user?.name} />}
-        <h2>{user?.name}</h2>
-        <ul>
-          {Object.keys(user).map((objkey, i) => <li key={i}>{objkey}:{user[objkey]
-          }</li>)}
-        </ul>
-      </article>
+    <div className='column'>
+      {isAuthenticated ? (
+        <div>
+          <h2>{user?.name}</h2>
+          {user?.picture && <img src={user.picture} alt={user?.name} />}
+          <ul>
+            {Object.keys(user).map((objKey, i) => {
+              if (objKey !== 'picture' && objKey !== 'name') {
+                return <li key={i}>{objKey}: {user[objKey]}</li>;
+              }
+              return null;
+            })}
+          </ul>
+          <button onClick={() => logout({ returnTo: window.location.origin })}>
+            Sign Out
+          </button>
+        </div>
+      ) : (
+        <div>
+          <h2>Auth0</h2>
+          <p>Please sign in to view your profile.</p>
+        </div>
+      )}
+    </div>
+  );
+};
 
-    )
-   
-  )
-}
-
-export default Profile
+export default Profile;
